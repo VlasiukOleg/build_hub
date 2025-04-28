ddc31b1ba76bc10ef5b3da283709b13ffc5cd0ff
Oleg <ikslprint@gmail.com>
2025-04-28 12:46:51 +0300

    fix phone number

--- src/components/ui/Phone/Phone.tsx
+++ src/components/ui/Phone/Phone.tsx
@@ -3,7 +3,7 @@ import { FaSquarePhone } from 'react-icons/fa6';
 const Phone: React.FC = () => {
   return (
     <a
-      href="tel:+380632790437"
+      href="tel:+380667196074"
       className="flex gap-1 items-center text-xs font-semibold md:text-lg"
     >
       <FaSquarePhone className="size-6 md:size-5 text-green-500" />
