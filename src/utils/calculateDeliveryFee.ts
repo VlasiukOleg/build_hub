045fcc7db22468dc76f30bba55f11942e92e8e08
Oleg <ikslprint@gmail.com>
2025-07-09 14:37:45 +0300

    fix moving price

--- src/utils/calculateDeliveryFee.ts
+++ src/utils/calculateDeliveryFee.ts
@@ -1,14 +1,16 @@
 export const calculateDeliveryFee = (weight: number) => {
-  if (weight > 0 && weight <= 2050) {
+  if (weight > 0 && weight <= 1500) {
     return 800;
-  }  else if (weight > 2050 && weight <= 3000) {
-    return 1200;
-  } else if (weight > 3000 && weight <= 5000) {
+  } else if (weight > 1500 && weight <= 2050) {
+    return 1000;
+  } else if (weight > 2050 && weight <= 3000) {
     return 1500;
+  } else if (weight > 3000 && weight <= 5000) {
+    return 1800;
   } else if (weight > 5000 && weight <= 10000) {
-    return 2000;
+    return 2300;
   } else if (weight > 10000) {
-    return 3000;
+    return 3400;
   } else {
     return 0;
   }
